fec289833c88915de5b79fe12eac6a12a3c1204c
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-06-07 18:32:16 -0400

    Updated css, index and javascript files

--- script.js
+++ script.js
@@ -44,24 +44,24 @@ var quizQuestions = [
 ]
 
 // Countdown timer function
-var timerEl= $("#timeLeft");
+var timerEl = $("#timeLeft");
 let timeLeft = 50;
+var timer;
 
-function startTime(){
-    timer = setInterval(function(){
+function startTime() {
+    timer = setInterval(function () {
         timeLeft--;
-        if(timeLeft===0){
+        if (timeLeft === 0) {
             alert("Time up");
             $("#question-page").hide();
-            $("#summary-page").show();
-            clearInterval(timer);
+            endQuiz();
         }
         timerEl.text(timeLeft);
     }, 1000);
 }
 
 //Event handler for when start button is clicked
-$("#start-btn").on('click', function(event){
+$("#start-btn").on('click', function (event) {
     event.preventDefault();
     $("#start-page").hide();
     startTime();
@@ -70,44 +70,79 @@ $("#start-btn").on('click', function(event){
 });
 
 // Function to display questions and answers
-function displayQuestions(){
+function displayQuestions() {
     let q = quizQuestions[qindex];
-    $('#question-page ul').html("")
+    $('#question-page ul').html("");
     $("#Question").text(q.question);
-    for(var i=0; i<q.answers.length; i++){
-        $('#options').append("<li><button class='btn btn-primary btn-lg' id=btn'"+i
-        + "'>" + q.answers[i]+ "</button></li>") 
+    for (var i = 0; i < q.answers.length; i++) {
+        let button = $("<button class='btn btn-primary btn-md m-1'></button>");
+        button.css('list-style-type', 'square');
+        button.attr("value", q.answers[i]);
+        button.text(q.answers[i]);
+        button.click(check);
+        var List = $("<li></li>")
+        List.append(button);
+        $('#options').append(List);
     }
 }
 
- //method to check if answer is correct after button is clicked
- function check(choice){
+//method to check if answer is correct after button is clicked
+function check() {
     let ans = quizQuestions[qindex];
-    if(ans.correctAns===choice){
-        score+=1;
+    if (this.value === ans.correctAns) {
+        score += 1;
         $("#correct").text("Correct!");
-    }else{
-        timeLeft-=10;
-        timerEl.text(timeLeft);
+    } else {
         $("#correct").text("Wrong!");
+        timeLeft -= 10;
+        timerEl.text(timeLeft);
     }
     qindex++;
 
-    if(qindex > quizQuestions.length){
-        //showSummary
-    }else{
+    if (qindex === quizQuestions.length) {
+        endQuiz();
+    } else {
         displayQuestions();
     }
- }
+}
 
- //event handler for when an option is selected
- $("#options").on('click', 'button', function(){
-    $('.choice').removeClass('choice');
-    $(this).addClass('choice');
+//function to display the end page after quiz is done
+function endQuiz() {
+    clearInterval(timer);
+    $("#start-page").hide();
+    $("#question-page").hide();
+    $("#end-page").show();
+    $("#score").text(score);
+    $("#qLength").text(quizQuestions.length);
+}
 
-    if($('button.choice').length){
-        let choice=parseInt($('button.choice').prop('id'));
-        console.log(choice);
-        check(choice);
+//function to save the highscore
+function saveScore() {
+    let hscore = JSON.parse(localStorage.getItem("Highscore"));
+    let name=$("#info").val()
+    let uScore = score
+
+    if(hscore==null){
+        hscore=[];
     }
- })
+    let info = {
+        userName: name,
+        userScore: uScore
+    };
+
+        hscore.push(info);
+        hscore.sort((a,b)=>b.userScore-a.userScore);
+        localStorage.setItem('Highscore', JSON.stringify(hscore));
+        window.location.replace("highscore.html")
+    
+}
+
+$("#submitbtn").on('click', function (e){
+    e.preventDefault();
+    saveScore();
+})
+
+$("#userInfo").on("submit", function(e){
+    e.preventDefault();
+    saveScore();
+})
